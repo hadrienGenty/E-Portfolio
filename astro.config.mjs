782d426e6ac7a62be48dc32bd00f0c4f5d4c0303
hadrienGenty <161524650+hadrienGenty@users.noreply.github.com>
2024-03-01 00:05:47 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,3 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import { defineConfig } from 'astro/config'
+export default defineConfig({
+  site: 'https://hadrienGenty.github.io',
+  base: '/portfolio',
+})
